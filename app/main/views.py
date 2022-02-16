1d3b54c875c27f599bf860bf25d6db61a46ad160
james-muriithi <muriithijames556@gmail.com>
2022-02-16 12:40:52 +0300

    added single comment page

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,7 @@ def index():
 @main.route('/notifications')
 def notifications():
     return render_template('notifications.html')
+
+@main.route('/notifications/<id>')
+def notification_show(id):
+    return render_template('single-news-page.html')    
