311edb49e1f0058a161b6a2a6d2a62a406dc4e6e
email <kibetdavidro@gmail.com>
2022-02-17 11:17:25 +0300

    add authentication files

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,13 @@
-from flask import render_template
 from . import main
+from flask import render_template, url_for, abort, request, redirect, flash
+from flask_login import login_required, current_user
+from ..models import User
+from .. import db
+from ..emails import mail_message
+
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    """main view function"""
+
+    return render_template("index.html")
