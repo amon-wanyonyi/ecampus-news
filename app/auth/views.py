8a47fa41edbb7f4c488d202e56081be81cbde35f
james-muriithi <muriithijames556@gmail.com>
2022-02-17 16:46:47 +0300

    added auth changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,7 +23,7 @@ def register():
 
         login_user(new_post)
         flash("Your account was created successfully","success")
-        return redirect(request.args.get('next') or url_for('main.index'))
+        return redirect(request.args.get('next') or url_for('main.navigations'))
 
     return render_template('auth/register.html')
 
@@ -38,21 +38,14 @@ def login():
         user=User.query.filter_by(email=post_email).first()
         if user is not None and user.verify_password(post_password):
             login_user(user)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('main.notifications'))
         flash("Incorrect credentials","error")
 
     return render_template('auth/login.html')
-
-
-@auth.route('/logout')
-
 
 @auth.route('/logout')
 @login_required
 def logout():
     """Logout function"""
     logout_user()
-    return redirect(url_for('.register'))
-
-
-
+    return redirect(url_for('main.index'))
