7ea8d95197c98f5701a8cadefeb1314c59428aa8
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-17 22:15:30 +0300

    Add new changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from . import main
 
-@main.route('/')
+@main.route('/about')
 def index():
-    return render_template('index.html')
+    return render_template('about.html')
