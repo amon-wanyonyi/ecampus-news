e90f56e63fa7aee60a0ad1f68fb7f69a00110741
james-muriithi <muriithijames556@gmail.com>
2022-02-17 14:28:32 +0300

    added notification create

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from . import main
 from flask import render_template, url_for, abort, request, redirect, flash
 from flask_login import login_required, current_user
-from ..models import User
+from ..models import Notification, User
 from .. import db
 from ..emails import mail_message
+from .forms import BlogForm
 
 
 @main.route('/')
@@ -11,10 +12,29 @@ def index():
     return render_template('index.html')
 
 @main.route('/notifications')
+@login_required
 def notifications():
-    return render_template('notifications.html')
+    notifications = Notification.get_all_notifications()
+    pinned_notifications = Notification.get_pinned_notifications()
+    return render_template('notifications.html', notifications=notifications, pinned_notifications=pinned_notifications)
 
 @main.route('/notifications/<id>')
 def notification_show(id):
     return render_template('single-news-page.html')    
 
+@main.route('/notification/create', methods=['POST'])
+@login_required
+def notification_create():
+    form = BlogForm()
+
+    if form.validate_on_submit():
+
+        blog = Notification(title=form.title.data, content=form.content.data,
+                      user=current_user, pinned=form.pinned.data)
+
+        db.session.add(blog)
+        db.session.commit()
+
+        flash("Notification added successfully", "success")
+
+    return redirect(request.referrer or url_for('main.index'))  
