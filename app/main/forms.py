17f7cda1cf72169b8d29f65481072a1c37579145
james-muriithi <muriithijames556@gmail.com>
2022-02-17 14:28:32 +0300

    added notification create

---
+++ app/main/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, StringField, IntegerField, EmailField,BooleanField
+from wtforms.validators import InputRequired
+
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    content = StringField('Content', validators=[InputRequired()])
+    pinned = BooleanField('Pinned')
+
+class EditBlogForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    category = IntegerField('Category', validators=[InputRequired()])
+    content = StringField('Content', validators=[InputRequired()])
+    pinned = BooleanField('Pinned')
+
+# comment form
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a Comment', validators=[InputRequired()])
+
+
+class ProfileForm(FlaskForm):
+    """Profile form"""
+    email = EmailField('Email',validators=[InputRequired()])
+    name = StringField('Name',validators=[InputRequired()])
+    about = TextAreaField('About')
--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    notifications = db.relationship('Notification', backref="user", lazy="dynamic")
+    comments = db.relationship('Comment', backref="user", lazy="dynamic")
 
     @property
     def password(self):
@@ -51,3 +53,71 @@ class Role(db.Model):
     name = db.Column(db.String(255))
 
     users = db.relationship('User', backref="role", lazy="dynamic")
+
+class Notification(db.Model):
+    '''
+    Notifications table
+    '''
+    __tablename__ = 'notifications'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(255))
+    content = db.Column(db.String())
+    pinned = db.Column(db.Boolean, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    comments = db.relationship('Comment', backref='notification', lazy='dynamic')
+
+    @property
+    def formatted_time(self):
+        from datetime import datetime
+        return self.created_at.strftime("%b %d, %Y")
+
+    @classmethod
+    def get_all_notifications(cls):
+        return cls.query.order_by(cls.created_at.desc()).all()
+
+    @classmethod
+    def get_pinned_notifications(cls, limit=3):
+        return cls.query.order_by(cls.created_at.desc()).filter_by(pinned=True).limit(limit).all()    
+
+    @classmethod
+    def delete_notification(cls, id):
+        blog = Notification.query.filter_by(id=id).first()
+        db.session.delete(blog)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Notification {self.title}'
+
+
+class Comment(db.Model):
+    '''comments table'''
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    notification_id = db.Column(db.Integer, db.ForeignKey('notifications.id'))
+    comment = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)  
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @property
+    def formatted_time(self):
+        from datetime import datetime
+        return self.created_at.strftime("%b %d, %Y")    
+
+     # delete comment
+    @classmethod
+    def delete_comment(cls, id):
+        comment = Comment.query.filter_by(id=id).first()
+        db.session.delete(comment)
+        db.session.commit()
+
+    # get all comments created by other users on my posts only 
+    @classmethod
+    def get_my_posts_comments(cls, user_id):
+        comments = Comment.query.filter(Comment.blog.has(user_id=user_id))
+        return comments        
